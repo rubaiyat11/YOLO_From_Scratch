d3d65f8843ce302991609399c16cb772e4d7d369
Weeeeeeeeeeee <rubaiyatabdullahrabi71@gmail.com>
2026-07-14 20:58:20 +0600

    Visualizing Feature Maps for a cat image

--- src/01_model_overview.py
+++ src/01_model_overview.py
@@ -1,20 +1,34 @@
 from ultralytics import YOLO
+from torchvision import transforms
+from PIL import Image
 import torch
 import matplotlib.pyplot as plt
+
 
 model = YOLO("yolo11n.pt")
 net = model.model
 
-# Fake image
-x = torch.randn(1, 3, 640, 640)
+# Load real image
+image = Image.open("../images/cat.jpg").convert("RGB")
+
+transform = transforms.Compose([
+    transforms.Resize((640, 640)),
+    transforms.ToTensor()
+])
+
+x = transform(image).unsqueeze(0)
 
 # First convolution
 x = net.model[0](x)
 
 print(x.shape)
 
-# Show first feature map
-plt.imshow(x[0, 0].detach().numpy(), cmap="gray")
-plt.title("Feature Map 0")
-plt.axis("off")
+fig, axes = plt.subplots(4, 4, figsize=(10, 10))
+
+for i, ax in enumerate(axes.flat):
+    ax.imshow(x[0, i].detach().numpy(), cmap="gray")
+    ax.set_title(f"Map {i}")
+    ax.axis("off")
+
+plt.tight_layout()
 plt.show()
