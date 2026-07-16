ba70fad02887104bed879542b7c4369f5b2c4f43
Weeeeeeeeeeee <rubaiyatabdullahrabi71@gmail.com>
2026-07-16 21:55:13 +0600

    Inspecting first convlation fature maps and activation statistics

--- src/01_model_overview.py
+++ src/01_model_overview.py
@@ -5,13 +5,13 @@ import torch
 import matplotlib.pyplot as plt
 
 
-model = YOLO("yolo11n.pt")
-net = model.model
+model = YOLO("yolo11n.pt")      #Instantiating yolo v11 model
+net = model.model               #Model Stats
 
 # Load real image
 image = Image.open("../images/cat.jpg").convert("RGB")
 
-transform = transforms.Compose([
+transform = transforms.Compose([   #Reshaping into usable ratio
     transforms.Resize((640, 640)),
     transforms.ToTensor()
 ])
@@ -20,6 +20,18 @@ x = transform(image).unsqueeze(0)
 
 # First convolution
 x = net.model[0](x)
+
+print("\nFeature Map Statistics")
+
+for i in range(16):
+    fmap = x[0, i]
+
+    print(
+        f"Map {i:02d} | "
+        f"Min: {fmap.min():.3f} | "
+        f"Max: {fmap.max():.3f} | "
+        f"Mean: {fmap.mean():.3f}"
+    )
 
 print(x.shape)
 
